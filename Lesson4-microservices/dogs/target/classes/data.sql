e160893195aae2a563cb67938fc44d8f7feb9651
Masoud Naji <63078848+masoud-naji@users.noreply.github.com>
2020-11-30 00:20:03 -0800

    Add files via upload

---
+++ Lesson4-microservices/dogs/target/classes/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO dog (id, name, description,origin) VALUES (1, 'jasper','A popular book','iran');
+INSERT INTO dog (id, name, description,origin) VALUES (2, 'candy','A favorite candy','iran');
+INSERT INTO dog (id, name, description,origin) VALUES (3, 'toy','A simple toy','iran');
+INSERT INTO dog (id, name, description,origin) VALUES (4, 'food','Your favorite food item','iran');
+INSERT INTO dog (id, name, description,origin) VALUES (5, 'tv','A flat screen TV','iran');
